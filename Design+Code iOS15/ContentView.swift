2850eb0a2aa76a25922f12734a922a629d211de4
Justin747 <justin747@gmail.com>
2021-11-20 16:26:10 -0800

    Initial Commit

---
+++ Design+Code iOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Design+Code iOS15
+//
+//  Created by Justin747 on 11/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Design+Code iOS15/Design_Code_iOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  Design_Code_iOS15App.swift
+//  Design+Code iOS15
+//
+//  Created by Justin747 on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Design_Code_iOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
