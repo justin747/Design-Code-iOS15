e4426c14aa8443d93e692760fee809de8051e86a
Justin747 <justin747@gmail.com>
2021-11-26 15:07:47 -0800

    Refactored Button and Gestures
    Refactored Button and Gestures so they are able to be re-used in local components
    
    Added gesture for pins/favorites

--- Design+Code iOS15/AccountView.swift
+++ Design+Code iOS15/AccountView.swift
@@ -10,95 +10,127 @@ import SwiftUI
 struct AccountView: View {
     
     @State var isDeleted: Bool = false
-    
+    @State var isPinned: Bool = false
     
     var body: some View {
         NavigationView {
             List {
-                VStack (spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(Image(systemName: "hexagon")
-                                        .symbolVariant(.fill)
-                                        .foregroundColor(.blue)
-                                        .font(.system(size: 200))
-                                        .offset(x: -50, y: -100))
-                    
-                    Text("CM Punk")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.small)
-                        
-                        Text("Los Angeles")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                
-                Section {
-                    NavigationLink {
-                        ContentView()
-                    } label: {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    NavigationLink {
-                        Text("Billing")
-                    } label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink {
-                        Text("Help")
-                    } label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
-                
-                Section {
-                    Link(destination: URL(string: "https://twitter.com/CMPunk")!) {
-                        HStack {
-                            Label("Website", systemImage: "house")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    if !isDeleted {
-                        Link(destination: URL(string: "https://www.youtube.com/c/AEW/featured")!) {
-                            HStack {
-                                Label("YouTube", systemImage: "tv")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                                    .listSectionSeparator(.hidden)
-                            }
-                        }
-                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                            Button(action: {isDeleted = true}) {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            Button {} label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                        }
-                    }
-                }
-                .accentColor(.primary)
+                profile
+                menu
+                links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+    
+    //MARK: Profile
+    
+    var profile: some View {
+        
+        VStack (spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100))
+            
+            Text("CM Punk")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                
+                Text("Los Angeles")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    //MARK: Menu
+    
+    var menu: some View {
+        
+        Section {
+            NavigationLink {
+                ContentView()
+            } label: {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink {
+                Text("Billing")
+            } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink {
+                Text("Help")
+            } label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+        
+    }
+    
+    //MARK: Links
+    
+    var links: some View {
+        
+        Section {
+            Link(destination: URL(string: "https://twitter.com/CMPunk")!) {
+                HStack {
+                    Label("Website", systemImage: "house")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+            if !isDeleted {
+                Link(destination: URL(string: "https://www.youtube.com/c/AEW/featured")!) {
+                    HStack {
+                        Label("YouTube", systemImage: "tv")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                            .listSectionSeparator(.hidden)
+                    }
+                }
+                .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                    Button(action: {isDeleted = true}) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    
+                    pinButton
+                }
+            }
+        }
+        .accentColor(.primary)
+    }
+    
+    var pinButton: some View {
+        Button { isPinned.toggle() } label: {
+            if isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(isPinned ? .gray : .yellow)
     }
 }
 
