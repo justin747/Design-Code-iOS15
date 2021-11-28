5d381996b16259575caba5b959956e63542114e4
Justin747 <justin747@gmail.com>
2021-11-27 21:21:12 -0800

    Added Enum and Switch statements
    Added Enum for Tab Bar items and Switch statement to handle selected tabs

--- Design+Code iOS15/TabBar.swift
+++ Design+Code iOS15/TabBar.swift
@@ -8,23 +8,46 @@
 import SwiftUI
 
 struct TabBar: View {
+    
+    @State var selectedTab: Tab = .home
+    
     var body: some View {
         ZStack(alignment: .bottom) {
-            ContentView()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Group {
+                switch selectedTab {
+                case .home:
+                    ContentView()
+                case .explore:
+                    AccountView()
+                case .notifications:
+                    AccountView()
+                case .library:
+                    AccountView()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            
+            
             HStack {
                 Spacer()
                 ForEach(tabItems) { item in
-                    VStack(spacing: 0) {
-                        Image(systemName: item.icon)
-                            .symbolVariant(.fill)
-                            .font(.body.bold())
-                            .frame(width: 44, height: 28)
-                        Text(item.text)
-                            .font(.caption2)
-                            .lineLimit(1)
+                    Button {
+                        selectedTab = item.tab
+                    } label: {
+                        VStack(spacing: 0) {
+                            Image(systemName: item.icon)
+                                .symbolVariant(.fill)
+                                .font(.body.bold())
+                                .frame(width: 44, height: 28)
+                            Text(item.text)
+                                .font(.caption2)
+                                .lineLimit(1)
+                        }
+                        .frame(maxWidth: .infinity)
                     }
-                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
                 }
             }
             .padding(.horizontal, 8)
