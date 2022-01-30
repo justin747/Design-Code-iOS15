4b80decab644ebc5ac202ef93b5b69a8f69be4b1
Justin747 <justin747@gmail.com>
2022-01-30 15:41:16 -0800

    Removed Hard-coded courses
    Removed Hard-coded courses and used sample data from Courses Struct

--- Design+Code iOS15/Components/FeaturedItem.swift
+++ Design+Code iOS15/Components/FeaturedItem.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct FeaturedItem: View {
+    var course: Course = courses[0]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
-            Image("Logo 2")
+            Image(course.logo)
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
@@ -19,15 +21,15 @@ struct FeaturedItem: View {
                 .padding(9)
                 .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
                 .strokeStyle(cornerRadius: 16)
-            Text("Swift for iOS 15")
+            Text(course.title)
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            Text("20 Sections - 3 Hours")
+            Text(course.subtitle)
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .foregroundStyle(.secondary)
-            Text(/*@START_MENU_TOKEN@*/"Build an iOS app for iOS 15 with custom layouts, animations, and ..."/*@END_MENU_TOKEN@*/)
+            Text(course.text)
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
@@ -43,8 +45,8 @@ struct FeaturedItem: View {
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .strokeStyle(cornerRadius: 30)
         .padding(.horizontal, 20.0)
-        ).overlay(
-            Image("Illustration 5")
+        .overlay(
+            Image(course.image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(height: 230)
--- Design+Code iOS15/Views/HomeView.swift
+++ Design+Code iOS15/Views/HomeView.swift
@@ -8,46 +8,59 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State var hasScroll: Bool = false
+    @State var hasScrolled = false
     
     var body: some View {
-        ScrollView {
-            GeometryReader { proxy in
-//                Text("\(proxy.frame(in: .named("scroll")).minY)")
-                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
-            }
-            .frame(height: 0)
+        ZStack {
+            Color("Background").ignoresSafeArea()
             
-            TabView {
-                ForEach(0 ..< 5) { item in
-                    FeaturedItem()
+            ScrollView {
+                scrollDetection
+                
+                featured
+                
+                Color.clear.frame(height: 1000)
+            }
+            .coordinateSpace(name: "scroll")
+            .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+                withAnimation(.easeInOut) {
+                    if value < 0 {
+                        hasScrolled = true
+                    } else {
+                        hasScrolled = false
+                    }
                 }
-            }
-            .tabViewStyle(.page(indexDisplayMode: .never))
-            .frame(height: 430)
-            .background(
-                Image("Blob 1")
-                    .offset(x: 250, y: -100)
-            
-            Color.clear.frame(height: 1000)
+            })
+            .safeAreaInset(edge: .top, content: {
+                Color.clear.frame(height: 70)
+            })
+            .overlay(
+                NavigationBar(title: "Featured", hasScroll: $hasScrolled)
+        )
         }
-        .coordinateSpace(name: "scroll")
-        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-            withAnimation(.easeInOut) {
-                if value < 0 {
-                    hasScroll = true
-                } else {
-                    hasScroll = false
+    }
+    
+    var scrollDetection: some View {
+        GeometryReader { proxy in
+//                Text("\(proxy.frame(in: .named("scroll")).minY)")
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }
+        .frame(height: 0)
+    }
+    
+    var featured: some View {
+        TabView {
+            ForEach(courses) { item in
+                GeometryReader {
+                    FeaturedItem(course: item)
                 }
             }
-            
-        })
-        .safeAreaInset(edge: .top, content: {
-            Color.clear.frame(height: 70)
-        })
-        .overlay(
-            NavigationBar(title: "Test", hasScroll: $hasScroll)
-                .opacity(hasScroll ? 1 : 0)
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
         )
     }
 }
