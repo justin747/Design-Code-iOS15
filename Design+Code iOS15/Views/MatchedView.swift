1e9c2a72faf21f287fb3a5832390db28583ce2df
Justin747 <justin747@gmail.com>
2022-01-31 21:21:01 -0800

    Created Matched View
    Created Matched View for custom transition animation

---
+++ Design+Code iOS15/Views/MatchedView.swift
@@ -0,0 +1,20 @@
+//
+//  MatchedView.swift
+//  Design+Code iOS15
+//
+//  Created by Justin747 on 1/31/22.
+//
+
+import SwiftUI
+
+struct MatchedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatchedView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchedView()
+    }
+}
