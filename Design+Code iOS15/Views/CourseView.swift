26bc2a07be0a95ade7b2af731c7b210be1c5647a
Justin747 <justin747@gmail.com>
2022-02-02 16:52:03 -0800

    Refactored Course
    Refactored Course. Moved from Home View to separate CourseView

---
+++ Design+Code iOS15/Views/CourseView.swift
@@ -0,0 +1,20 @@
+//
+//  CourseView.swift
+//  Design+Code iOS15
+//
+//  Created by Justin747 on 2/2/22.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseView()
+    }
+}
