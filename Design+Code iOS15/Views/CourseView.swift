2dc8caaf863a75faef30d0923994a7a9b5ffb2ee
Justin747 <justin747@gmail.com>
2022-02-06 14:00:44 -0800

    Added Left Swipe gesture
    Added Left Swipe gesture that will dismiss Course View.
    
    Also added blur effect to left swipe gesture

--- Design+Code iOS15/Views/CourseView.swift
+++ Design+Code iOS15/Views/CourseView.swift
@@ -15,6 +15,7 @@ struct CourseView: View {
     @Binding var show: Bool
     @State var appear = [false, false, false]
     @EnvironmentObject var model: Model
+    @State var viewState: CGSize = .zero
     
     var body: some View {
         
@@ -28,6 +29,24 @@ struct CourseView: View {
                     .opacity(appear[2] ? 1 : 0)
             }
             .background(Color("Background"))
+            .mask(RoundedRectangle(cornerRadius: viewState.width / 3, style: .continuous))
+            .shadow(color: .black.opacity(0.3), radius: 30, x: 0, y: 10)
+            .scaleEffect(viewState.width / -500 + 1)
+            .background(.black.opacity(viewState.width / 500))
+            .background(.ultraThinMaterial)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        guard value.translation.width > 0 else { return }
+                        
+                        viewState = value.translation
+                    }
+                    .onEnded { value in
+                        withAnimation(.closeCard) {
+                            viewState = .zero
+                        }
+                    }
+            )
             .ignoresSafeArea()
             
             button
@@ -40,7 +59,7 @@ struct CourseView: View {
         }
     }
     
-        
+    
     var cover: some View {
         
         GeometryReader { proxy in
@@ -77,20 +96,20 @@ struct CourseView: View {
         .frame(height: 500)
     }
     
-        var content: some View {
-            VStack(alignment: .leading, spacing: 30) {
-                Text("SwiftUI is hands-down the best way for designers to take a first step into code. ")
-                    .font(.title3).fontWeight(.medium)
-                Text("This course")
-                    .font(.title).bold()
-                Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
-                Text("This year, SwiftUI got major upgrades from the WWDC 2020. The big news is that thanks to Apple Silicon, Macs will be able to run iOS and iPad apps soon. SwiftUI is the only framework that allows you to build apps for all of Apple's five platforms: iOS, iPadOS, macOS, tvOS and watchOS with the same codebase. New features like the Sidebar, Lazy Grid, Matched Geometry Effect and Xcode 12's visual editing tools will make it easier than ever to build for multiple platforms.")
-                Text("Multiplatform app")
-                    .font(.title).bold()
-                Text("For the first time, you can build entire apps using SwiftUI only. In Xcode 12, you can now create multi-platform apps with minimal code changes. SwiftUI will automatically translate the navigation, fonts, forms and controls to its respective platform. For example, a sidebar will look differently on the Mac versus the iPad, while using exactly the same code. Dynamic type will adjust for the appropriate platform language, readability and information density. ")
-            }
-            .padding(20)
+    var content: some View {
+        VStack(alignment: .leading, spacing: 30) {
+            Text("SwiftUI is hands-down the best way for designers to take a first step into code. ")
+                .font(.title3).fontWeight(.medium)
+            Text("This course")
+                .font(.title).bold()
+            Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
+            Text("This year, SwiftUI got major upgrades from the WWDC 2020. The big news is that thanks to Apple Silicon, Macs will be able to run iOS and iPad apps soon. SwiftUI is the only framework that allows you to build apps for all of Apple's five platforms: iOS, iPadOS, macOS, tvOS and watchOS with the same codebase. New features like the Sidebar, Lazy Grid, Matched Geometry Effect and Xcode 12's visual editing tools will make it easier than ever to build for multiple platforms.")
+            Text("Multiplatform app")
+                .font(.title).bold()
+            Text("For the first time, you can build entire apps using SwiftUI only. In Xcode 12, you can now create multi-platform apps with minimal code changes. SwiftUI will automatically translate the navigation, fonts, forms and controls to its respective platform. For example, a sidebar will look differently on the Mac versus the iPad, while using exactly the same code. Dynamic type will adjust for the appropriate platform language, readability and information density. ")
         }
+        .padding(20)
+    }
     
     
     var button: some View {
@@ -99,7 +118,7 @@ struct CourseView: View {
             withAnimation(.closeCard) {
                 show.toggle()
                 model.showDetail.toggle()
-                    
+                
             }
         } label: {
             Image(systemName: "xmark")
@@ -114,44 +133,44 @@ struct CourseView: View {
     }
     
     var overlayContent: some View {
-            VStack(alignment: .leading, spacing: 12) {
-                Text(course.title)
-                    .font(.largeTitle.weight(.bold))
-                    .matchedGeometryEffect(id: "title\(course.id)", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text(course.subtitle)
-                    .font(.footnote.weight(.semibold))
-                    .matchedGeometryEffect(id: "subtitle", in: namespace)
-                Text(course.text)
+        VStack(alignment: .leading, spacing: 12) {
+            Text(course.title)
+                .font(.largeTitle.weight(.bold))
+                .matchedGeometryEffect(id: "title\(course.id)", in: namespace)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text(course.subtitle)
+                .font(.footnote.weight(.semibold))
+                .matchedGeometryEffect(id: "subtitle", in: namespace)
+            Text(course.text)
+                .font(.footnote)
+                .matchedGeometryEffect(id: "caption\(course.id)", in: namespace)
+            
+            
+            Divider()
+                .opacity(appear[0] ? 1 : 0)
+            HStack {
+                Image("CM Punk")
+                    .resizable()
+                    .frame(width: 26, height: 26)
+                    .cornerRadius(10)
+                    .padding(8)
+                    .background(.ultraThinMaterial, in:
+                                    RoundedRectangle(cornerRadius: 18, style: .continuous))
+                    .strokeStyle(cornerRadius: 18)
+                Text("Taught by CM Punk")
                     .font(.footnote)
-                    .matchedGeometryEffect(id: "caption\(course.id)", in: namespace)
-                
-                
-                Divider()
-                    .opacity(appear[0] ? 1 : 0)
-                HStack {
-                    Image("CM Punk")
-                        .resizable()
-                        .frame(width: 26, height: 26)
-                        .cornerRadius(10)
-                        .padding(8)
-                        .background(.ultraThinMaterial, in:
-                                        RoundedRectangle(cornerRadius: 18, style: .continuous))
-                        .strokeStyle(cornerRadius: 18)
-                    Text("Taught by CM Punk")
-                        .font(.footnote)
-                }
-                .opacity(appear[1] ? 1 : 0)
             }
-                .padding(20)
-                .background(
-                    Rectangle()
-                        .fill(.ultraThinMaterial)
-                        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                        .matchedGeometryEffect(id: "blur\(course.id)", in: namespace)
-                )
-                .offset(y: 250)
-                .padding(20)
+            .opacity(appear[1] ? 1 : 0)
+        }
+        .padding(20)
+        .background(
+            Rectangle()
+                .fill(.ultraThinMaterial)
+                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .matchedGeometryEffect(id: "blur\(course.id)", in: namespace)
+        )
+        .offset(y: 250)
+        .padding(20)
     }
     
     func fadeIn() {
@@ -171,7 +190,7 @@ struct CourseView: View {
         appear[1] = true
         appear[2] = true
     }
-
+    
 }
 
 
