dc20289ccb7f85214685aff8d3b8423484b66b29
Justin747 <justin747@gmail.com>
2022-02-06 14:28:55 -0800

    Fixed Course View drag gesture bug
    Fixed bug in left swipe drag gesture that caused issues when left swipe was diagonal instead of horizontal

--- Design+Code iOS15/Views/CourseView.swift
+++ Design+Code iOS15/Views/CourseView.swift
@@ -16,6 +16,7 @@ struct CourseView: View {
     @State var appear = [false, false, false]
     @EnvironmentObject var model: Model
     @State var viewState: CGSize = .zero
+    @State var isDraggable = true
     
     var body: some View {
         
@@ -34,19 +35,7 @@ struct CourseView: View {
             .scaleEffect(viewState.width / -500 + 1)
             .background(.black.opacity(viewState.width / 500))
             .background(.ultraThinMaterial)
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        guard value.translation.width > 0 else { return }
-                        
-                        viewState = value.translation
-                    }
-                    .onEnded { value in
-                        withAnimation(.closeCard) {
-                            viewState = .zero
-                        }
-                    }
-            )
+            .gesture(isDraggable ? drag : nil)
             .ignoresSafeArea()
             
             button
@@ -173,6 +162,33 @@ struct CourseView: View {
         .padding(20)
     }
     
+    var drag: some Gesture {
+        DragGesture(minimumDistance: 30, coordinateSpace: .local)
+            .onChanged { value in
+                guard value.translation.width > 0 else { return }
+                
+                if value.startLocation.x < 100 {
+                    withAnimation(.closeCard) {
+                        viewState = value.translation
+                    }
+                }
+                
+                if viewState.width > 120 {
+                    close()
+                }
+            }
+            .onEnded { value in
+                if viewState.width > 80 {
+                    close()
+                } else {
+                    
+                    withAnimation(.closeCard) {
+                        viewState = .zero
+                    }
+                }
+            }
+    }
+    
     func fadeIn() {
         withAnimation(.easeOut.delay(0.3)) {
             appear[0] = true
@@ -189,6 +205,19 @@ struct CourseView: View {
         appear[0] = true
         appear[1] = true
         appear[2] = true
+    }
+    
+    func close() {
+        
+        withAnimation(.closeCard.delay(0.3)) {
+            show.toggle()
+            model.showDetail.toggle()
+        }
+        
+        withAnimation(.closeCard) {
+            viewState = .zero
+        }
+        isDraggable = false
     }
     
 }
