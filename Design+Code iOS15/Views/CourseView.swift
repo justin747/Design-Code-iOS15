ed8fed1bf81fe5574fbe179126792bed3cf8f38a
Justin747 <justin747@gmail.com>
2022-02-10 19:51:50 -0800

    Added frame to Course Image
    Added frame to Course Image so image doesn't get too large on devices with large screens

--- Design+Code iOS15/Views/CourseView.swift
+++ Design+Code iOS15/Views/CourseView.swift
@@ -63,6 +63,8 @@ struct CourseView: View {
                 Image(course.image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                    .padding(20)
+                    .frame(maxWidth: 500)
                     .matchedGeometryEffect(id: "image\(course.id)", in: namespace)
                     .offset(y: scrollY > 0 ? scrollY * -0.8 : 0)
             )
